c9866f7ea830c0d40ab977275df5d41bfa40b308
Tyler Phillips <tylerfphillips@gmail.com>
2013-10-24 11:17:27 -0400

    Google Search images added, problem with lyrics and song in player not matching up was resolved

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,36 +4,47 @@ function loadLyrics(data) {
 
   var currentSong = 0
 
-  // fake js
-  $('.next').click(function(){
+  $('.next-song').click(function(){
   	var newUrl = 'https://embed.spotify.com/?uri=' + songs[currentSong].spotifyURI;
+  	
   	$('#player').attr('src', newUrl)
-  	currentSong ++;
+  	
+  	
 
   	$.getJSON("http://lyrics.wikia.com/api.php?callback=?",
+  	
   	{
   	   func: 'getSong',
   	   artist: songs[currentSong].artistName,
   	   song: songs[currentSong].songName,
   	   fmt: 'realjson' 
   	},
+
   	function(data){
   	  $('#lyrics').text(data.lyrics);
 
+  	  window.imageSearch = new google.search.ImageSearch();
+  	  // Set searchComplete as the callback function when a search is 
+  	  // complete.  The imageSearch object will have results in it.
+  	  imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+  	  // find the lyrics
+  	  imageSearch.execute(data.lyrics);
+  	  
+  	  // Include the required Google branding
+  	  google.search.Search.getBranding('branding');
   	});
+
+  	currentSong ++;
+
   })
 
   var songs = [
-    // {
-    //   spotifyURI: 'spotify:track:2JFmTONwQdRaOj2s2DhFah',
-    //   artistName: 'Ben Howard',
-    //   songName: 'Keep Your Head Up',
-      
-      // spotifyURI: 'spotify:track:2JFmTONwQdRaOj2s2DhFah',
-      // artistName: 'Ben Howard',
-      // songName: 'Keep Your Head Up',
-      // lyricLink: 'http://lyrics.wikia.com/api.php?artist=Ben_Howard&song=Keep_Your_Head_Up'
-    // },
+    {
+      spotifyURI: 'spotify:track:2JFmTONwQdRaOj2s2DhFah',
+      artistName: 'Ben Howard',
+      songName: 'Keep Your Head Up',
+    }, 
     {
       spotifyURI: 'spotify:track:2dLLR6qlu5UJ5gk0dKz0h3',
       artistName: 'Lorde',
@@ -56,11 +67,6 @@ function loadLyrics(data) {
       songName: 'Battles'
     },
     {
-      spotifyURI: 'spotify:track:60rHc4AkLlP4XVSATvBb6K',
-      artistName: 'Halestorm',
-      songName: 'I Miss The Misery'
-    },
-    {
       spotifyURI: 'spotify:track:3Kj2EWpIBnvETsYq4cq0IH',
       artistName: 'Gregory Alan Isakov',
       songName: 'Big Black Car'
@@ -70,116 +76,35 @@ function loadLyrics(data) {
       artistName: 'Dawes',
       songName: 'When My Time Comes'
     },
-    
-  	// {
-   //    spotifyURI: "spotify:track:17vkimYLXZzpCiFpI3GVA7",
-   //    artistName: "Jessica Brando",
-   //    songName: "Karma Police"
-   //  },
-   //  {
-   //    spotifyURI: "spotify:track:7qmCTTSYXiCMe7jboDOk6e",
-   //    artistName: "The Beautiful South",
-   //    songName: "Don't Fear The Reaper"
-   //  },
     {
       spotifyURI: "spotify:track:3bbdnFcNSHPaYrOzx7dJeE",
       artistName: "Pheonix",
       songName: "Lisztomania"
     },
-    // {
-    //   spotifyURI: "spotify:track:7hExqd5aeA6cdDFx6sBfd3",
-    //   artistName: "Pheonix",
-    //   songName: "1901"
-    // },
+    
   ]
 
   
 
 
-// $.get("https://embed.spotify.com/?uri=" + songs[0].spotifyURI)
+$.get("https://embed.spotify.com/?uri=" + songs[0].spotifyURI)
 
-// google.load('search', '1');
+google.load('search', '1');
 
-//       var imageSearch;
+function searchComplete() {
+	$('#content').html('')
+	// Check that we got results
+	if (imageSearch.results && imageSearch.results.length > 0) {
 
-//       function addPaginationLinks() {
-      
-//         // To paginate search results, use the cursor function.
-//         var cursor = imageSearch.cursor;
-//         var curPage = cursor.currentPageIndex; // check what page the app is on
-//         var pagesDiv = document.createElement('div');
-//         for (var i = 0; i < cursor.pages.length; i++) {
-//           var page = cursor.pages[i];
-//           if (curPage == i) { 
+	  // Loop through our results, printing them to the page.
+	  var results = imageSearch.results;
+	  for (var i = 0; i < results.length; i++) {
+	    // For each result write it's title and image to the screen
+	    var result = results[i]
+	    var image = $('<img src="'+ result.tbUrl + '">')
+	    $('#content').append(image)
+	  }
+	}
+}
 
-//           // If we are on the current page, then don't make a link.
-//             var label = document.createTextNode(' ' + page.label + ' ');
-//             pagesDiv.appendChild(label);
-//           } else {
-
-//             // Create links to other pages using gotoPage() on the searcher.
-//             var link = document.createElement('a');
-//             link.href="/image-search/v1/javascript:imageSearch.gotoPage("+i+');';
-//             link.innerHTML = page.label;
-//             link.style.marginRight = '2px';
-//             pagesDiv.appendChild(link);
-//           }
-//         }
-
-//         var contentDiv = document.getElementById('content');
-//         contentDiv.appendChild(pagesDiv);
-//       }
-
-//       function searchComplete() {
-
-//         // Check that we got results
-//         if (imageSearch.results && imageSearch.results.length > 0) {
-		
-//           // Grab our content div, clear it.
-//           var contentDiv = document.getElementById('content');
-//           contentDiv.innerHTML = '';
-
-//           // Loop through our results, printing them to the page.
-//           var results = imageSearch.results;
-//           for (var i = 0; i < results.length; i++) {
-//             // For each result write it's title and image to the screen
-//             var result = results[i];
-//             var imgContainer = document.createElement('div');
-//             var title = document.createElement('div');
-            
-//             // We use titleNoFormatting so that no HTML tags are left in the 
-//             // title
-//             title.innerHTML = result.titleNoFormatting;
-//             var newImg = document.createElement('img');
-
-//             // There is also a result.url property which has the escaped version
-//             newImg.src="/image-search/v1/result.tbUrl;"
-//             imgContainer.appendChild(title);
-//             imgContainer.appendChild(newImg);
-
-//             // Put our title + image in the content
-//             contentDiv.appendChild(imgContainer);
-//           }
-
-//           // Now add links to additional pages of search results.
-//           addPaginationLinks(imageSearch);
-//         }
-//       }
-
-//       function OnLoad() {
-      
-//         // Create an Image Search instance.
-//         imageSearch = new google.search.ImageSearch();
-
-//         // Set searchComplete as the callback function when a search is 
-//         // complete.  The imageSearch object will have results in it.
-//         imageSearch.setSearchCompleteCallback(this, searchComplete, null);
-
-//         // Find me a beautiful car.
-//         imageSearch.execute(songs[0].songName + songs[0].artistName);
-        
-//         // Include the required Google branding
-//         google.search.Search.getBranding('branding');
-//       }
-//       google.setOnLoadCallback(OnLoad);
 
