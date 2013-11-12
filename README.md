b2da301efe3c531ea1b58ac6caad0e229a48e0bb
Tyler Phillips <tylerfphillips@gmail.com>
2013-11-12 10:37:12 -0500

    Updated ReadMe

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spotify Visual Lyrics 
+
+## Overview
+
+Spotify Visual Lyrics is an open-source project that will enhance your experience as you listen to music. 
+
+Enjoy as Google image search randomly finds pictures based off lyrics of the song playing.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
-});
-
 function loadLyrics(data) {
     console.log(data)
   }
@@ -60,9 +56,14 @@ function loadLyrics(data) {
       songName: 'Big Black Car'
     },
     {
+      spotifyURI: 'spotify:track:4h8VwCb1MTGoLKueQ1WgbD',
+      artistName: 'Avicii',
+      songName: 'Wake Me Up'
+    },
+    {
       spotifyURI: 'spotify:track:2JFmTONwQdRaOj2s2DhFah',
       artistName: 'Ben Howard',
-      songName: 'Keep Your Head Up',
+      songName: 'Keep Your Head Up'
     }, 
     {
       spotifyURI: "spotify:track:3bbdnFcNSHPaYrOzx7dJeE",
@@ -70,40 +71,19 @@ function loadLyrics(data) {
       songName: "Lisztomania"
     },
     {
-      spotifyURI: 'spotify:track:2dLLR6qlu5UJ5gk0dKz0h3',
-      artistName: 'Lorde',
-      songName: 'Royals',
-      lyricLink: 'http://lyrics.wikia.com/api.php?artist=lorde&song=royals'
-    }, 
-    {
-      spotifyURI: 'spotify:track:7s0lDK7y3XLmI7tcsRAbW0',
-      artistName: 'Fall Out Boy',
-      songName: 'My Songs Know What You Did In The Dark (Light Em Up)'
-    },
-    {
       spotifyURI: 'spotify:track:2QepprWju53OwtTHrpnLo9',
       artistName: 'Goldfrapp',
       songName: 'Happiness'
     }, 
-    {
-      spotifyURI: 'spotify:track:3G9ETaH55bMQx8hwNhAgbU',
-      artistName: 'Gregory Alan Isakov',
-      songName: 'The Stable Song'
-    },
     {
       spotifyURI: 'spotify:track:4xjC014uAzL5O8xD1Tp6NC',
       artistName: 'Hudson Taylor',
       songName: 'Battles'
     },
     {
-      spotifyURI: 'spotify:track:4KXzKRp4amS8unfZaxCGS9',
-      artistName: 'Delta Rae',
-      songName: 'Morning Comes'
-    },
-    {
-      spotifyURI: 'spotify:track:5PUvinSo4MNqW7vmomGRS7',
-      artistName: 'Robin Thicke',
-      songName: 'Blurred Lines'
+      spotifyURI: "spotify:track:4o0NjemqhmsYLIMwlcosvW",
+      artistName: "Band Of Horses",
+      songName: "The Funeral"
     },
     {
       spotifyURI: 'spotify:track:0kzfqqvipRSBQchrB3xX8D',
@@ -111,14 +91,34 @@ function loadLyrics(data) {
       songName: 'When My Time Comes'
     },
     {
-      spotifyURI: "spotify:track:4o0NjemqhmsYLIMwlcosvW",
-      artistName: "Band Of Horses",
-      songName: "The Funeral"
+      spotifyURI: 'spotify:track:2dLLR6qlu5UJ5gk0dKz0h3',
+      artistName: 'Lorde',
+      songName: 'Royals'
+    }, 
+    {
+      spotifyURI: 'spotify:track:5PUvinSo4MNqW7vmomGRS7',
+      artistName: 'Robin Thicke',
+      songName: 'Blurred Lines'
     },
-  {
+    {
       spotifyURI: "spotify:track:0MOzmbwL6Aad4jIOJomWVm",
       artistName: "Bon Iver",
       songName: "Towers"
+    },
+    {
+      spotifyURI: 'spotify:track:3G9ETaH55bMQx8hwNhAgbU',
+      artistName: 'Gregory Alan Isakov',
+      songName: 'The Stable Song'
+    },
+    {
+      spotifyURI: 'spotify:track:4KXzKRp4amS8unfZaxCGS9',
+      artistName: 'Delta Rae',
+      songName: 'Morning Comes'
+    },
+    {
+      spotifyURI: 'spotify:track:7s0lDK7y3XLmI7tcsRAbW0',
+      artistName: 'Fall Out Boy',
+      songName: 'My Songs Know What You Did In The Dark (Light Em Up)'
     },
 
   ]
@@ -152,40 +152,6 @@ function searchComplete() {
     }; 
 }
 
-        // $('#content').append(image)
 
-    // bxslider code
-      // $(image.appendTo(''.bxslider li''))
-      // $('.bxslider').append(image)
-      
-      // console.log(image)
-      // $('.bxslider li').bxslider()
-      // $('.bxslider').bxslider('refresh')
-
-  // Slidejs code
-    // if (window.slidesjsReady) {
-    //   $("#content").slidesjs("refresh");
-    // } else {
-    //   $("#content").slidesjs()
-    //   window.slidesjsReady = true
-    // }
-
-// $(function(){
-//   var slideShowContainer = $('.slideShow'),
-//       slideShowItem = '.slides li',
-//       transitionTime = 1000,
-//       pauseTime = 3000,
-//       curSlide = 0,
-//       int = setInterval(function(){
-//       if(curSlide === parseInt($(slideShowItem).length - 1)){
-//         curSlide = 0;
-//       } else {
-//         curSlide++;
-//       }
-  
-//       slideShowContainer.animate({ scrollLeft: $(slideShowItem).width() * curSlide }, transitionTime);
-//   }, pauseTime);
-
-// })
 
 
